302ff94c28d683337a66707a418553501fbf4797
Shniuras <seimurinho@gmail.com>
2017-11-12 11:51:46 +0200

    Files

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Fk Stickers BLOG'as</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="nav">
+          <a class="nav-link active" href="index.php">Home</a>
+          <a class="nav-link" href="posts.php">All Posts</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="blog-header">
+      <div class="container">
+        <div class="logo"><img src="images/logo.png" width="100px" height="100px"></div>
+        <h1 class="blog-title">FK Stickers BLOG'as</h1>
+        <p class="lead blog-description">Naujienos, Transferiai, Zaidimu momentai ir daugiau</p>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="row">
---
+++ posts.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php"; ?>
+        <div class="col-sm-8 blog-main">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Fk Stickers išvyka į Rudiškes </h2>
+            <p class="blog-post-meta">Rugsėjo 6, 2017 <a href="#">Shniuras</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <a href="post.php?id=1" class="readmore">Daugiau</a>
+          </div><!-- /.blog-post -->
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Fk Stickers 0 - 2 Tėvynes Sąjunga</h2>
+            <p class="blog-post-meta">Rugsėjo 4, 2017 <a href="#">Shniuras</a></p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <a href="post.php?id=1" class="readmore">Daugiau</a>
+          </div><!-- /.blog-post -->
+
+        </div><!-- /.blog-main -->
+<?php include "includes/footer.php"; ?>
